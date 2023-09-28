b6918230322b5be432891f115fed4063397c7ac4
bellobambo <bellobambo21@gmail.com>
2023-09-28 23:21:12 +0100

    test

--- Data-Structure/queue.js
+++ Data-Structure/queue.js
@@ -1,44 +1,167 @@
-class Queue {
-  constructor() {
-    this.items = [];
+// class Queue {
+//   constructor() {
+//     this.items = [];
+//   }
+//   enqueue(element) {
+//     this.items.push(element);
+//   }
+//   dequeue() {
+//     return this.items.shift();
+//   }
+//   isEmpty() {
+//     return this.items.length === 0;
+//   }
+
+//     peek() {
+//         if (!this.isEmpty()) {
+//             return this.items[0]
+//         }
+//         return null
+//     }
+
+//     size() {
+//         return this.items.length
+//     }
+
+//     print() {
+//         console.log(this.items.toString)
+//     }
+// }
+
+// const queue = new Queue
+// console.log(queue.isEmpty)
+
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+
+// console.log(queue.size())
+// queue.print()
+
+// console.log(queue.dequeue());
+
+// class Queue {
+//   constructor() {
+//     this.items = {};
+//     this.rear = 0;
+//     this.front = 0;
+//   }
+
+//   enqueue(element) {
+//     this.items[this.rear] = element;
+//     this.rear++;
+//   }
+
+//   dequeue(element) {
+//     const item = this.items[this.front];
+//     delete this.items[this.front];
+//     this.front++;
+//     return item;
+//   }
+
+//   isEmpty() {
+//     return this.rear - this.front === 0;
+//   }
+
+//   peek() {
+//     return this.items[this.front];
+//   }
+
+//   size() {
+//     return this.rear - this.front;
+//   }
+
+//   print() {
+//     console.log(this.items);
+//   }
+// }
+
+// const queue = new Queue()
+// console.log(queue.isEmpty())
+
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+// console.log(queue.size())
+// queue.print()
+
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.rear = -1;
+    this.front = -1;
   }
-  enqueue(element) {
-    this.items.push(element);
+
+  isFull() {
+    return this.currentLength === this.capacity;
   }
-  dequeue() {
-    return this.items.shift();
-  }
+
   isEmpty() {
-    return this.items.length === 0;
+    return this.currentLength === 0;
   }
 
-    peek() {
-        if (!this.isEmpty()) {
-            return this.items[0]
-        }
-        return null
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.rear = this.rear + 1;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
     }
+  }
 
-    size() {
-        return this.items.length
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
     }
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = this.front + 1;
+    this.currentLength -= 1;
+    if (this.isEmpty()) {
+      this.front = -1;
+      this.rear = -1;
+    }
+    return item;
+  }
 
-    print() {
-        console.log(this.items.toString)
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[this.front];
     }
+    return null;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("Queue is Empty");
+    } else {
+      let i = this.front;
+      let str = "";
+
+      do {
+        str += this.items[i] + " ";
+        i = (i + 1) % this.capacity;
+      } while (i !== (this.rear + 1) % this.capacity);
+
+      console.log(str);
+    }
+  }
 }
 
-const queue = new Queue
-console.log(queue.isEmpty)
+
+const queue = new CircularQueue(5)
+console.log(queue.isEmpty())
 
 queue.enqueue(10)
 queue.enqueue(20)
 queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
 
-
-console.log(queue.size())
+console.log(queue.isFull())
 queue.print()
 
 console.log(queue.dequeue());
-
-
